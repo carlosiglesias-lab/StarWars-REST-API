afe46e665f3768b28fb9a447fb09fdace8984bff
carlosiglesias-lab <carlos.igles@gmail.com>
2023-03-29 07:53:56 +0000

    terminado

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, Users, Characters, Episodes, Locations,Favorites
 #from models import Person
 
 app = Flask(__name__)
@@ -36,14 +36,77 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/users', methods=['GET'])
+def get_all_users():
+    users = Users.query.all()
+    user_serialized = [x.serialize() for x in users]
+    return jsonify({"Usuarios:" : user_serialized}), 200 
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/users/favorites', methods=['GET'])
+def get_all_users():    
+    user_id = request.json.get("user_id") 
+    favorites = Favorites.query.filter_by(id_user = user_id)
+    favorites_serialized = [x.serialize() for x in favorites]
+    return jsonify({"Favoritos de Usuario:" : favorites_serialized}), 200 
 
-    return jsonify(response_body), 200
+@api.route('/favorites/characters/<id>', methods=['POST'])
+def new_character_fav(id):
+    user_id = request.json.get("user_id")    
+    db.session.add(Favorites (user_id=user_id, id_character=id))
+    db.session.commit()
+    return jsonify({"response": "Character added to favorites succesfully"}), 200
+
+@api.route('/favorites/characters/<id>', methods=['DELETE'])
+def delete_character_favorite(id):
+    user_id = request.json.get("user_id") 
+    favorite =  Favorites.query.filter_by(id_user=user_id,id_character=id).first()
+    db.session.delete(favorite)    
+    db.session.commit()
+    return jsonify({"response": "Favorite deleted successfully"}), 200  
+
+@api.route('/favorites/locations/<id>', methods=['POST'])
+def new_character_fav(id):
+    user_id = request.json.get("user_id")    
+    db.session.add(Favorites (user_id=user_id, id_location=id))
+    db.session.commit()
+    return jsonify({"response": "Location added to favorites succesfully"}), 200
+
+@api.route('/favorites/location/<id>', methods=['DELETE'])
+def delete_location_favorite(id):
+    user_id = request.json.get("user_id") 
+    favorite =  Favorites.query.filter_by(id_user=user_id,id_location=id).first()
+    db.session.delete(favorite)    
+    db.session.commit()
+    return jsonify({"response": "Favorite deleted successfully"}), 200
+
+@app.route('/characters', methods=['GET'])
+def all_characters():
+    characters = Characters.query.all()
+    character_serialized = [x.serialize() for x in characters]
+    return jsonify({"Characters:" : character_serialized}), 200
+
+@app.route('/characters/<id>', methods=['GET'])
+def single_charactere(id):    
+    character = Characters.query.get(id)    
+    return jsonify({"Character:" : character.serialize()}), 200
+
+@app.route('/episodes', methods=['GET'])
+def all_episodes():
+    episodes = Episodes.query.all()
+    episodes_serialized = [x.serialize() for x in episodes]
+    return jsonify({"Episodios:" : episodes_serialized}), 200
+
+@app.route('/episodes/<id>', methods=['GET'])
+def single_episode(id):    
+    episode = Episodes.query.get(id)    
+    return jsonify({"Character:" : episode.serialize()}), 200
+
+@app.route('/locations', methods=['GET'])
+def all_locations():
+    locations = Locations.query.all()
+    location_serialized = [x.serialize() for x in locations]
+    return jsonify({"Locations:" : location_serialized}), 200
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -2,18 +2,110 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    favorites = db.relationship("Favorites", backref="User")
+    
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return self.email
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "email": self.email,            
+        }
+
+class Characters(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    gender = db.Column(db.String(80), unique=False, nullable=False)
+    alive = db.Column(db.Boolean(), unique=False, nullable=False, default=True)
+    episode = db.relationship("Episodes", secondary="character_has_episodes", backref="Characters")
+    location = db.relationship("Locations", secondary="character_has_locations", backref="Characters")
+    favorites = db.relationship("Favorites", backref="Characters")
+    
+    def __repr__(self):
+        return self.name
+   
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "gender" : self.gender,
+            "alive" : self.alive
+            
+        }
+class Episodes(db.Model):
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    lenght = db.Column(db.Integer)    
+    location = db.relationship("Locations", secondary="episode_has_locations", backref="Episodes")
+    favorites = db.relationship("Favorites", backref="Episodes")
+
+    def __repr__(self):
+        return self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "lenght" : self.lenght,           
+            
+        }
+
+class Locations(db.Model):
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    type = db.Column(db.String(250))           
+    favorites = db.relationship("Favorites", backref="Locations")
+
+    def __repr__(self):
+        return self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "type" : self.type,           
+            
+        }
+
+
+character_has_episodes= db.Table("character_has_episodes",
+    db.Column("character_id", db.Integer, db.ForeignKey('characters.id'),nullable=False , primary_key=True),
+    db.Column("episode_id", db.Integer, db.ForeignKey('episodes.id'),nullable=False , primary_key=True)
+)
+
+character_has_locations= db.Table("character_has_locations",
+    db.Column("character_id", db.Integer, db.ForeignKey('characters.id'),nullable=False , primary_key=True),
+    db.Column("location_id", db.Integer, db.ForeignKey('locations.id'),nullable=False , primary_key=True)
+)
+      
+    
+episode_has_locations= db.Table("episode_has_locations",
+    db.Column("episode_id", db.Integer, db.ForeignKey('episodes.id'),nullable=False , primary_key=True),
+    db.Column("location_id", db.Integer, db.ForeignKey('locations.id'),nullable=False , primary_key=True)
+)
+    
+
+class Favorites(db.Model):
+    
+    id = db.Column(db.Integer, primary_key=True)
+    id_user = db.Column(db.Integer, db.ForeignKey('users.id'))
+    id_character = db.Column(db.Integer,db.ForeignKey('characters.id'))
+    id_episode = db.Column(db.Integer, db.ForeignKey('episodes.id'))
+    id_location = db.Column(db.Integer, db.ForeignKey('locations.id'))   
+
+    def serialize(self):
+        return {
+            "id_user": self.id_user,
+            "id_character": self.id_character,
+            "id_episode" : self.id_episode,           
+            "id_location" : self.id_location,
         }
